9dc9eae5e5d13ea369bd91b0a15c22324d747b1b
HanifahAR06 <hanifahaulia0611@gmail.com>
2020-04-20 21:13:15 +0700

    pull ke github

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,23 +1,21 @@
 <?php 
  
-class Home extends CI_Controller 
-{     
-	function __construct()     
-	{         
-		parent::__construct();         
-		$this->load->model(['ModelBuku', 'ModelUser', 'ModelBooking']);     
-	} 
+class Home extends CI_Controller {     
+    function __construct()     
+    {         
+        parent::__construct();         
+        $this->load->model(['ModelBuku', 'ModelUser', 'ModelBooking']);     } 
  
     public function index()     
     {         
-    	$data = [             
-    		'judul' => "Katalog Buku",             
-    		'buku' => $this->ModelBuku->getBuku()->result(), 
+        $data = [             
+            'judul' => "Katalog Buku",             
+            'buku' => $this->ModelBuku->getBuku()->result(), 
      ]; 
  
         //jika sudah login dan jika belum login         
      if ($this->session->userdata('email')) {             
-     	$user = $this->ModelUser->cekData(['email' => $this->session>userdata('email')])->row_array(); 
+        $user = $this->ModelUser->cekData(['email' => $this->session>userdata('email')])->row_array(); 
  
             $data['user'] = $user['nama']; 
  
@@ -26,11 +24,11 @@ class Home extends CI_Controller
             $this->load->view('templates/templates-user/modal');             
             $this->load->view('templates/templates-user/footer', $data);         
         } else {             
-        	$data['user'] = 'Pengunjung';             
-        	$this->load->view('templates/templates-user/header', $data);             
-        	$this->load->view('buku/daftarbuku', $data);             
-        	$this->load->view('templates/templates-user/modal');             
-        	$this->load->view('templates/templates-user/footer', $data);         
+            $data['user'] = 'Pengunjung';             
+            $this->load->view('templates/templates-user/header', $data);             
+            $this->load->view('buku/daftarbuku', $data);             
+            $this->load->view('templates/templates-user/modal');             
+            $this->load->view('templates/templates-user/footer', $data);         
         }     
     } 
     public function detailBuku()     
